0b55c1f264e9e1bd5e6eaf9523c8d5b6a1386e30
Akkawat Tanan <kop_akkawat@hotmail.com>
2016-10-10 02:16:56 +0700

    create home routing and change aungular2.ppt

--- angular2-basic-v1/src/app/app.module.ts
+++ angular2-basic-v1/src/app/app.module.ts
@@ -3,9 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//import { Routes, RouterModule } from '@angular/router';
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
+
+//const appRoutes: Routes = [
+  // เราจะนิยาม Route หรือเส้นทางของเราในนี้
+  // เช่น
+  // { path: 'pages', component: PageListComponent },
+  // เพื่อบอกว่าเมื่อไหร่ที่เข้ามาจาก /pages ให้วิ่งไปใช้บริการคอมโพแนนท์ชื่อ PageListComponent 
+//]
 
 @NgModule({
   declarations: [
@@ -16,7 +25,9 @@ import { HeaderComponent } from './shared/header/header.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
+    //RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
